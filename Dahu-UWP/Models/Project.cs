8f01334da5bcfb25e7cc7f0ccf8955848ba6bb66
Debrabelge <Thomas-db@hotmail.fr>
2017-10-16 00:14:54 +0200

    16102017 Save

---
+++ Dahu-UWP/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dahu_UWP.Models
+{
+    class Project
+    {
+        
+        [JsonProperty(PropertyName = "FooBar")]
+        public string Foo { get; set; }
+        [JsonIgnore]
+        public DateTime LastModified { get; set; }
+
+    }
+}
---
+++ Dahu-UWP/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dahu_UWP.Models
+{
+    class User
+    {
+    }
+}
