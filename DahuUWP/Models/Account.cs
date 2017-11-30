a3f9d1f537edfd70f0e6a2e4e2aecc4bf16cc05e
Debrabelge <Thomas-db@hotmail.fr>
2017-11-30 12:20:13 +0100

    Stars rating set up

--- DahuUWP/Models/Account.cs
+++ DahuUWP/Models/Account.cs
@@ -1,6 +1,7 @@
 ﻿using DahuUWP.Services;
 using GalaSoft.MvvmLight;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,8 @@ namespace DahuUWP.Models
     public class Account : ObservableObject
     {
         private string mail;
+
+        [JsonProperty(PropertyName = "mail")]
         public string Mail
         {
             get { return mail; }
@@ -29,6 +32,8 @@ namespace DahuUWP.Models
         }
 
         private string password;
+
+        [JsonProperty(PropertyName = "password")]
         public string Password
         {
             get { return password; }
@@ -45,14 +50,13 @@ namespace DahuUWP.Models
         [JsonProperty(PropertyName = "id")]
         public int Id { get; set; }
 
-        [JsonProperty(PropertyName = "mail")]
-        public string Mail2 { get; set; }
 
-        [JsonProperty(PropertyName = "password")]
-        public string Password2 { get; set; }
-
+        private string _uuid;
         [JsonProperty(PropertyName = "uuid")]
-        public string Uuid { get; set; }
+        public string Uuid { get { return _uuid; } set { _uuid = value; } }
+
+        private string _token;
+        public string Token { get { return _token; } set { _token = value; } }
 
         [JsonProperty(PropertyName = "cgu")]
         public int Cgu { get; set; }
@@ -80,15 +84,20 @@ namespace DahuUWP.Models
 
                 object connection = new
                 {
-                    mail = (string)AppStaticInfo.Account.GetType().GetProperty("Mail").GetValue(AppStaticInfo.Account),
-                    password = (string)AppStaticInfo.Account.GetType().GetProperty("Password").GetValue(AppStaticInfo.Account)
+                    mail = (string)AppStaticInfo.Account.Mail,
+                    password = (string)AppStaticInfo.Account.Password
                 };
                 HttpResponseMessage result = service.Post(connection, "auth");
                 string responseBody = result.Content.ReadAsStringAsync().Result;
+                var resp = (Newtonsoft.Json.Linq.JObject)JsonConvert.DeserializeObject(responseBody);
+                
+                AppStaticInfo.Account.Uuid = (string) resp["data"]["uuid"];
+                AppStaticInfo.Account.Token = (string) resp["data"]["_token"]; //r.GetType().GetProperty("_token").GetValue(resp);
                 return true;
             }
-            catch //(Exception e) //Task<bool> task = Task.Run<bool>(async () => await service.PostAsync());
+            catch (Exception ex) //Task<bool> task = Task.Run<bool>(async () => await service.PostAsync());
             {
+                System.Diagnostics.Debug.Fail(ex.ToString());
                 return false;
             }
         }
---
+++ DahuUWP/Views/Components/Inputs/StarRating.xaml.cs
@@ -0,0 +1,141 @@
+﻿using DahuUWP.Utils.Converter;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace DahuUWP.Views.Components.Inputs
+{
+    public sealed partial class StarRating : UserControl
+    {
+        private int NbStars = 5;
+        
+
+        public StarRating()
+        {
+            Stars = new ObservableCollection<object>();
+            DataContext = this;
+            this.InitializeComponent();
+            CreateStars(3);
+            //List<TodoItem> items = new List<TodoItem>();
+            //items.Add(new TodoItem() { StarIcon = "star-full" });
+            //items.Add(new TodoItem() { StarIcon = "star-full" });
+            //items.Add(new TodoItem() { StarIcon = "star-full" });
+
+            //starList.ItemsSource = items;
+            //ColumnDefinition column = new ColumnDefinition();
+            //colum
+            //StarRatingGrid.ColumnDefinitions.Add();
+        }
+
+        //public List<object> Stars { get; set; }
+
+        public ObservableCollection<object> Stars
+        {
+            get;
+            set;
+        }
+
+        public void CreateStars(int score)
+        {
+            int it = 0;
+
+            //feeling full stars
+            while (it < score && it < NbStars)
+            {
+                object star = new
+                {
+                    StarIcon = IconConverter.IconToImageBrush("star-full")
+                };
+                Stars.Add(star);
+                it++;
+            }
+            //feeling empty stars
+            while (it < NbStars)
+            {
+                object star = new
+                {
+                    StarIcon = IconConverter.IconToImageBrush("star-empty")
+                };
+                Stars.Add(star);
+                it++;
+            }
+            StarList.DataContext = this.Stars;
+        }
+
+        private void ColorStarBeforEndIndex(int index)
+        {
+            int it = 0;
+
+            while (it < index && it < NbStars)
+            {
+                object star = new
+                {
+                    StarIcon = IconConverter.IconToImageBrush("star-full")
+                };
+                Stars[it] = star;
+                //StarList.Items.Insert(it + 1, star);
+                it++;
+            }
+            while (it < NbStars)
+            {
+                object star = new
+                {
+                    StarIcon = IconConverter.IconToImageBrush("star-empty")
+                };
+                Stars[it] = star;
+                //StarList.Items.Insert(it, star);
+                it++;
+            }
+        }
+
+
+
+        public class TodoItem
+        {
+            public string StarIcon { get; set; }
+        }
+
+        private void Grid_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            
+        }
+
+        private void Grid_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            //object star = new
+            //{
+            //    StarIcon = IconConverter.IconToImageBrush("star-empty")
+            //};
+            //Stars.Add(star);
+            //List<object> stars = new List<object>();
+
+
+            var item = (sender as FrameworkElement).DataContext;
+            int index = StarList.Items.IndexOf(item);
+
+            //var star = new Grid()
+            //{
+            //    Background = IconConverter.IconToImageBrush("star-empty")
+            //};
+            //Stars.Remove(0);
+            //StarList.ItemsSource = Stars;
+            //stars.Add(star);
+            //StarList.ItemsSource = stars;
+            ColorStarBeforEndIndex(index);
+        }
+    }
+}
